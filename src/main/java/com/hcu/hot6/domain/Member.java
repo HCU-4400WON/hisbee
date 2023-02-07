d9b52f140a07bca556415f819c96fb578fe8ddf6
zionhann <hanzion1108@gmail.com>
2023-02-07 18:57:02 +0900

    :sparkles: 유저 엔티티 생성

---
+++ src/main/java/com/hcu/hot6/domain/Member.java
@@ -0,0 +1,41 @@
+package com.hcu.hot6.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private boolean isPublic;
+
+    @Enumerated(value = EnumType.STRING)
+    private Department department;
+
+    @Enumerated(value = EnumType.STRING)
+    private Position position;
+
+    private String bio;
+    private int grade;
+    private String club;
+    private String contact;
+    private String externalLinks;
+
+    @ManyToMany(mappedBy = "likes")
+    private List<Post> likes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts = new ArrayList<>();
+}
