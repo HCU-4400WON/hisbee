fcb9e7b604fc5e69cee06afc1cd211fc4bb3f770
zionhann <hanzion1108@gmail.com>
2023-02-07 18:45:17 +0900

    :sparkles: 학부, 포지션에 대한 enum type 추가

---
+++ src/main/java/com/hcu/hot6/domain/Department.java
@@ -0,0 +1,45 @@
+package com.hcu.hot6.domain;
+
+public enum Department {
+    // 글로벌리더십학부
+    GLS,
+
+    // 국제어문학부
+    INT_STUDY,
+
+    // 경영경제학부
+    MANAGEMENT,
+
+    // 법학부
+    LAW,
+
+    // 커뮤니케이션학부
+    COMMUNICATION,
+
+    // 상담심리사회복지학부
+    COUNSELING,
+
+    // 생명과학부
+    LIFE_SCIENCE,
+
+    // 공간환경시스템공학부
+    SPATIAL_ENV,
+
+    // 전산전자공학부
+    CSEE,
+
+    // 콘텐츠융합디자인학부
+    CCD,
+
+    // 기계제어공학부
+    MCE,
+
+    // ICT 창업학부
+    ICT,
+
+    // 창의융합교육원
+    CCE,
+
+    // AI 융합교육원
+    AI
+}
