c7f328f447ed71f297748063eba8852c380fa6a9
zionhann <hanzion1108@gmail.com>
2023-02-07 17:36:43 +0900

    :tada: Back-end initial setup

---
+++ src/main/java/com/hcu/hot6/Hot6Application.java
@@ -0,0 +1,13 @@
+package com.hcu.hot6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hot6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hot6Application.class, args);
+    }
+
+}
