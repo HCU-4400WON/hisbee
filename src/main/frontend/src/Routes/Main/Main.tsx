3e2bd1224c80e97bf215542d88c3b9065ef4ff3c
ohinhyuk <8156217@naver.com>
2023-02-09 00:26:06 +0900

    :wrench: [#17] 라이브러리 설치 및 컴포넌트들 생성
    테일윈드css , router , styled-component , typescript 설치 , main page , post page 생성

---
+++ src/main/frontend/src/Routes/Main/Main.tsx
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <button className="py-2 px-4 font-semibold rounded-lg shadow-md bg-gray-500 hover:bg-green-700">
+      Main
+    </button>
+  );
+}
+
+export default Main;
