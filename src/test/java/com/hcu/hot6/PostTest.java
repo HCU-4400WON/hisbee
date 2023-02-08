7bd8deaf13adc3d891d1ea4cec50c81172c92489
jang-yujin <jyj9747@naver.com>
2023-02-08 17:10:38 +0900

    :truck: [#14] MentoringTest -> PostTest 테스트 파일 이름 수정
    PostTest 파일에 Mentoring, Project, Study 관련 테스트 진행예정

--- src/test/java/com/hcu/hot6/PostTest.java
+++ src/test/java/com/hcu/hot6/PostTest.java
@@ -21,17 +21,13 @@ public class MentoringTest {
     @Test
     public void 멘토링_객체_생성() throws Exception{
         final Member member = new Member();
-        final Period period = Period.ByPeriodBuilder()
-                .postEnd(LocalDateTime.of(2023, 3, 2, 0, 0, 0))
-                .projectStart(LocalDateTime.of(2023, 3, 3, 0, 0, 0))
-                .projectEnd(LocalDateTime.of(2023, 6, 18, 0, 0, 0))
-                .build();
+        //final Period period =
 
         final Mentoring mentoring = Mentoring.builder()
                 .title("앱프로그래밍 과목 멘토링 해드립니다.")
                 .content("코딩을 처음 접해본 저는 앱프로그래밍 과목 들으면서 너무 어려웠어서 옆에서 도와주는 누군가가 있으면 좋겠다는 생각을 했었습니다. 멘토링을 받고싶은 분들 연락 주세요.")
                 .contact("22000123@handong.ac.kr")
-                .period(period)
+                //.period(period)
                 .author(member)
                 .maxMentor(0)
                 .maxMentee(3)
@@ -42,14 +38,27 @@ public class MentoringTest {
         Assertions.assertThat(mentoring.getTitle()).isEqualTo("앱프로그래밍 과목 멘토링 해드립니다.");
         Assertions.assertThat(mentoring.getContent()).isEqualTo("코딩을 처음 접해본 저는 앱프로그래밍 과목 들으면서 너무 어려웠어서 옆에서 도와주는 누군가가 있으면 좋겠다는 생각을 했었습니다. 멘토링을 받고싶은 분들 연락 주세요.");
         Assertions.assertThat(mentoring.getContact()).isEqualTo("22000123@handong.ac.kr");
-        Assertions.assertThat(mentoring.getPeriod().getPostEnd()).isEqualTo(LocalDateTime.of(2023, 3, 2, 0, 0, 0));
-        Assertions.assertThat(mentoring.getPeriod().getProjectStart()).isEqualTo(LocalDateTime.of(2023, 3, 3, 0, 0, 0));
-        Assertions.assertThat(mentoring.getPeriod().getProjectEnd()).isEqualTo(LocalDateTime.of(2023, 6, 18, 0, 0, 0));
-        Assertions.assertThat(mentoring.getAuthor()).isEqualTo(member);
+//        Assertions.assertThat(mentoring.getPeriod().getPostEnd()).isEqualTo(LocalDateTime.of(2023, 3, 2, 0, 0, 0));
+//        Assertions.assertThat(mentoring.getPeriod().getProjectStart()).isEqualTo(LocalDateTime.of(2023, 3, 3, 0, 0, 0));
+//        Assertions.assertThat(mentoring.getPeriod().getProjectEnd()).isEqualTo(LocalDateTime.of(2023, 6, 18, 0, 0, 0));
+//        Assertions.assertThat(mentoring.getAuthor()).isEqualTo(member);
         Assertions.assertThat(mentoring.getMaxMentor()).isEqualTo(0);
         Assertions.assertThat(mentoring.getMaxMentee()).isEqualTo(3);
         Assertions.assertThat(mentoring.isHasPay()).isEqualTo(false);
         Assertions.assertThat(mentoring.isCompleted()).isEqualTo(false);
         Assertions.assertThat(mentoring.getTotal()).isEqualTo(3);
+    }
+
+    @Test
+    public void 기간_객체_생성() throws Exception{
+        final Period period = Period.ByPeriodBuilder()
+                .postEnd(LocalDateTime.of(2023, 3, 2, 0, 0, 0))
+                .projectStart(LocalDateTime.of(2023, 3, 3, 0, 0, 0))
+                .projectEnd(LocalDateTime.of(2023, 6, 18, 0, 0, 0))
+                .build();
+
+        Assertions.assertThat(period.getPostEnd()).isEqualTo(LocalDateTime.of(2023, 3, 2, 0, 0, 0));
+        Assertions.assertThat(period.getProjectStart()).isEqualTo(LocalDateTime.of(2023, 3, 3, 0, 0, 0));
+        Assertions.assertThat(period.getProjectEnd()).isEqualTo(LocalDateTime.of(2023, 6, 18, 0, 0, 0));
     }
 }
