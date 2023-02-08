7d2295354a9dfdeeb45916bae383f644b59bb0cc
zionhann <hanzion1108@gmail.com>
2023-02-08 22:08:28 +0900

    :sparkles: [#11] 등록되지 않은 유저인 경우 기본 정보 등록

--- src/main/java/com/hcu/hot6/domain/Member.java
+++ src/main/java/com/hcu/hot6/domain/Member.java
@@ -1,27 +1,38 @@
 package com.hcu.hot6.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Builder
+    public Member(String uid, String email, String pictureUrl) {
+        this.uid = uid;
+        this.email = email;
+        this.pictureUrl = pictureUrl;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
+    private String uid;
+
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
-    private String nickname;
-
-    @Column(nullable = false)
-    private boolean isPublic;
+    private String pictureUrl;
 
     @Enumerated(value = EnumType.STRING)
     private Department department;
@@ -29,8 +40,10 @@ public class Member {
     @Enumerated(value = EnumType.STRING)
     private Position position;
 
+    private boolean isPublic = false;
+    private String nickname;
     private String bio;
-    private int grade;
+    private Integer grade;
     private String club;
     private String contact;
     private String externalLinks;
--- src/main/java/com/hcu/hot6/security/SecurityConfig.java
+++ src/main/java/com/hcu/hot6/security/SecurityConfig.java
@@ -1,12 +1,58 @@
 package com.hcu.hot6.security;
 
+import com.hcu.hot6.domain.Member;
+import com.hcu.hot6.oauth.OAuth2UserDetails;
+import com.hcu.hot6.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.Optional;
+
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint()
+                        .userService(this.oAuth2UserService()));
+        return http.build();
+    }
+
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+            OAuth2UserDetails userDetails = new OAuth2UserDetails(oAuth2User.getAttributes());
+            Optional<Member> registered = memberRepository.findMemberById(userDetails.getUid());
+
+            if (registered.isEmpty()) {
+                Member member = Member.builder()
+                        .uid(userDetails.getUid())
+                        .email(userDetails.getEmail())
+                        .pictureUrl(userDetails.getPicture())
+                        .build();
+                memberRepository.register(member);
+            }
+            return oAuth2User;
+        };
+    }
 
     // In development only
     @Bean
