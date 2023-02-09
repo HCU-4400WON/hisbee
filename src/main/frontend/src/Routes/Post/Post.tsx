fdd1d4933aeebb545420a7016c633f7577b49b18
ohinhyuk <8156217@naver.com>
2023-02-09 18:14:04 +0900

    :art: f-post/#26
    게시글 페이지 반응형 + 코드정리

--- src/main/frontend/src/Routes/Post/Post.tsx
+++ src/main/frontend/src/Routes/Post/Post.tsx
@@ -16,19 +16,21 @@ my-1
 border-b-2 
 border-black 
 item-center
+vertical-center
 `;
 
 const FilterTitle = tw.p`
-text-4xl 
-mt-2 
-mx-20
+block
+text-xl 
+mx-10
+my-auto
+w-20
+md:text-4xl 
+md:w-60
 `;
 
 const FilterButtonBox = tw.span`
-absolute
-left-96
-top-0
-bottom-0
+
 flex 
 items-center 
 text-lg
@@ -40,6 +42,54 @@ border-gray-300
 px-2 
 rounded-3xl
 mx-3
+`;
+
+const SortBox = tw.div`
+flex 
+justify-end 
+my-20 
+mx-10
+`;
+
+const SortTitle = tw.p`
+font-bold 
+mr-5
+`;
+
+const SortSelect = tw.select`
+  w-44
+`;
+
+const PostGrid = tw.div`
+grid 
+grid-cols-1
+gap-10 
+mx-10
+sm:grid-cols-2
+xl:grid-cols-4
+
+`;
+
+const PostBox = tw.div`
+h-80 
+w-200
+rounded-md
+
+`;
+
+const PostImage = tw.div`
+border-0 
+rounded-sm 
+h-2/5 
+mx-5 
+mt-5 
+mb-3 
+bg-gradient-to-r from-white to-purple-200 to-purple-300
+`;
+
+const PostContentFirstRow = tw.div`
+flex 
+items-center
 `;
 
 function Post() {
@@ -78,33 +128,31 @@ bg-gray-200"
         </FilterButtonBox>
       </FilterRow>
 
-      <div className="flex justify-end my-20 mx-10">
-        <p className="font-bold mr-5">Sort by</p>
-        <select className="w-44">
+      <SortBox>
+        <SortTitle>Sort by</SortTitle>
+        <SortSelect>
           <option>평점 순</option>
           <option>좋아요 순</option>
-        </select>
-      </div>
+        </SortSelect>
+      </SortBox>
 
-      <div className="grid grid-cols-4 gap-10 mx-10">
-        {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((e) => (
-          <div
-            className="h-80 rounded-md"
-            style={{ boxShadow: "0px 0px 25px rgb(0 0 0 / 0.3)" }}
-          >
-            <div className="border-0 rounded-sm h-2/5 mx-5 mt-5 mb-3 bg-gradient-to-r from-white to-purple-200 to-purple-300"></div>
-            <div className="flex items-center">
-              <p className="mx-5 text-sm font-bold">개발자</p>
+      <PostGrid>
+        {[0, 0, 0, 0].map((e) => (
+          <PostBox style={{ boxShadow: "0px 0px 25px rgb(0 0 0 / 0.3)" }}>
+            <PostImage />
+
+            <PostContentFirstRow>
+              <p className="mx-5 my-1 text-sm font-bold">개발자</p>
               <p className="text-sm text-blue-500">2명 모집</p>
-            </div>
+            </PostContentFirstRow>
             <p className="mx-5 my-1 text-lg font-bold">앱 개발 팀원 모집</p>
             <p className="mx-5 my-3">
               안녕하세요! 사이드 프로젝트 팀원을 구하고 있는...!!!
             </p>
             <span></span>
-          </div>
+          </PostBox>
         ))}
-      </div>
+      </PostGrid>
     </>
   );
 }
