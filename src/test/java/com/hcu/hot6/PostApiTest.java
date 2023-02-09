91849ff36c30c258fb5eba6925ae43bf96845a36
jang-yujin <jyj9747@naver.com>
2023-02-10 01:48:37 +0900

    :white_check_mark: [#23] 모집인원 음수, 필수 입력필드 누락BAD_REQUEST 테스트 통과
    모집 인원이 음수인 경우는 BAD_REQUEST, 모집인원을 실수로 request body에 담지 않은 경우는 엔티티 필드의 @NotNull 어노테이션에 따라 자동으로 0으로 세팅됨. 필수 입력 필드를 request body에 보내지 않은 경우는 BAD_REQUEST

--- src/test/java/com/hcu/hot6/PostApiTest.java
+++ src/test/java/com/hcu/hot6/PostApiTest.java
@@ -29,6 +29,9 @@ public class PostApiTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    /**
+     * Project, Study, Mentoring 각각, 정상 create 케이스 테스트
+     * */
     @DisplayName("멘토링 생성 API 테스트 : Normal")
     @Test
     public void createMentoring() throws Exception{
@@ -99,7 +102,6 @@ public class PostApiTest {
         request.setProjectStart(LocalDateTime.of(2023, 3, 10, 0, 0, 0));
         request.setProjectEnd(LocalDateTime.of(2023, 7, 2, 0, 0, 0));
         request.setAuthorId(1L);
-        request.setMaxDeveloper(3);
         request.setMaxMember(5);
 
         String url = "http://localhost:"+port+"/posts";
@@ -113,9 +115,15 @@ public class PostApiTest {
         Assertions.assertThat(response.getBody().getTitle().compareTo("제목 test") == 0);
     }
 
-    @DisplayName("스터디 생성 API 테스트 : Abnormal -> 필수 입력사항이 생략된 bad request")
+
+    /**
+     * Project, Study, Mentoring 각각, 모집 인원 음수인 경우 BAD_REQUEST 에러 나는지 테스트
+     * 각 필드에 필요한 max 모집 인원 하나라도 음수를 입력하면 BAD_REQUEST 에러가 난다.
+     * 하지만 실수로 필요한 max 모집 인원 필드를 채우지 않고 보냈을 때에는 엔티티의 @NotNull 어노테이션이 0으로 채워주기 때문에 정상 작동하는데에 문제가 없다.
+     * */
+    @DisplayName("max 모집 인원 수가 음수 이므로, bad request 에러가 나야 합니다.")
     @Test
-    public void 스터디_필수_생략() throws Exception{
+    public void 모집인원_음수_study() throws Exception{
         // 1L를 PK로 가지는 유저가 있는 상태에서 테스트. -> 통과
         PostCreationRequest request = new PostCreationRequest();
         request.setDtype("S");
@@ -126,14 +134,100 @@ public class PostApiTest {
         request.setProjectStart(LocalDateTime.of(2023, 3, 10, 0, 0, 0));
         request.setProjectEnd(LocalDateTime.of(2023, 7, 2, 0, 0, 0));
         request.setAuthorId(1L);
-        request.setMaxDeveloper(3);
-        request.setMaxMember(4);
+        request.setMaxMember(-1);
 
         String url = "http://localhost:"+port+"/posts";
 
         ResponseEntity<PostCreationResponse> response = restTemplate.postForEntity(url, request, PostCreationResponse.class);
 
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
+
+    @DisplayName("max 모집 인원 수가 음수 이므로, bad request 에러가 나야 합니다.")
+    @Test
+    public void 모집인원_음수_project() throws Exception{
+        PostCreationRequest request = new PostCreationRequest();
+        request.setDtype("P");
+        request.setTitle("제목 test");
+        request.setContent("내용 test");
+        request.setContact("연락처 test");
+        request.setPostEnd(LocalDateTime.of(2023, 3, 2, 0, 0, 0));
+        request.setProjectStart(LocalDateTime.of(2023, 3, 10, 0, 0, 0));
+        request.setProjectEnd(LocalDateTime.of(2023, 7, 2, 0, 0, 0));
+        request.setAuthorId(1L);
+        request.setHasPay(true);
+        request.setMaxDeveloper(-3);
+        request.setMaxPlanner(2);
+        request.setMaxDesigner(1);
+
+        String url = "http://localhost:"+port+"/posts";
+
+        ResponseEntity<PostCreationResponse> response = restTemplate.postForEntity(url, request, PostCreationResponse.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @DisplayName("max 모집 인원 수가 음수 이므로, bad request 에러가 나야 합니다.")
+    @Test
+    public void 모집인원_음수_mentoring() throws Exception{
+        PostCreationRequest request = new PostCreationRequest();
+        request.setDtype("M");
+        request.setTitle("제목 test");
+        request.setContent("내용 test");
+        request.setContact("연락처 test");
+        request.setPostEnd(LocalDateTime.of(2023, 3, 2, 0, 0, 0));
+        request.setProjectStart(LocalDateTime.of(2023, 3, 10, 0, 0, 0));
+        request.setProjectEnd(LocalDateTime.of(2023, 7, 2, 0, 0, 0));
+        request.setAuthorId(1L);
+        request.setHasPay(true);
+        request.setMaxMentor(-1);
+        request.setMaxMentee(4);
+
+        String url = "http://localhost:"+port+"/posts";
+
+        ResponseEntity<PostCreationResponse> response = restTemplate.postForEntity(url, request, PostCreationResponse.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 필수로 입력해야 하는 사항에 대해 입력하지 않은 경우 BAD_REQUEST 에러가 나야 한다.
+     * PostCreationRequest에서 사용한 Validation 어노테이션이 잘 작동하는지 확인.
+     * */
+    @DisplayName("@NotNull validation 체크 - BAD_REQUEST 에러가 나야 한다.")
+    @Test
+    public void notnull_validation() throws Exception{
+        // dtype, title, contact, authorId, postEnd, projectEnd, projectStart 각각 주석 처리 해가면서 체크하기
+        // String, String으로 포맷팅 되는 LocalDateTime은 validation 체크 가능하지만, hasPay는 Boolean 타입이라 여기서 validation 체크 불가능함
+        PostCreationRequest request = new PostCreationRequest();
+        request.setDtype("S");
+        //request.setDtype("P");
+        //request.setDtype("M");
+        request.setTitle("제목 test");
+        request.setContent("내용 test");
+        //request.setContact("연락처 test");
+        request.setPostEnd(LocalDateTime.of(2023, 3, 2, 0, 0, 0));
+        request.setProjectStart(LocalDateTime.of(2023, 3, 10, 0, 0, 0));
+        request.setProjectEnd(LocalDateTime.of(2023, 7, 2, 0, 0, 0));
+        request.setAuthorId(1L);
+        request.setHasPay(true);
+//        멘토링인 경우 아래 코드 활성화
+//        request.setMaxMember(5);
+//        프로젝트인 경우 아래 코드 활성화
+//        request.setMaxDeveloper(3);
+//        request.setMaxPlanner(2);
+//        request.setMaxDesigner(1);
+//        멘토링인 경우 아래 코드 활성화
+//        request.setMaxMentor(1);
+//        request.setMaxMentee(4);
+
+        String url = "http://localhost:"+port+"/posts";
+
+        ResponseEntity<PostCreationResponse> response = restTemplate.postForEntity(url, request, PostCreationResponse.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+
 
 }
