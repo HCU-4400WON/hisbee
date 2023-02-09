52259ef8c8a275755d463714b66a990f8861d4d8
jang-yujin <jyj9747@naver.com>
2023-02-09 18:30:42 +0900

    :white_check_mark: [#23] Post create API 테스트 코드 작성
    미완. Spring security 관련 코드 받기 전 commit

--- src/main/java/com/hcu/hot6/repository/MemberRepository.java
+++ src/main/java/com/hcu/hot6/repository/MemberRepository.java
@@ -1,2 +1,17 @@
-package com.hcu.hot6.repository;public class MemberRepository {
+package com.hcu.hot6.repository;
+
+import com.hcu.hot6.domain.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public Member findById(Long authorId) {
+        return em.find(Member.class, authorId);
+    }
 }
--- src/test/java/com/hcu/hot6/PostApiTest.java
+++ src/test/java/com/hcu/hot6/PostApiTest.java
@@ -1,2 +1,62 @@
-package com.hcu.hot6;public class PostApiTest {
+package com.hcu.hot6;
+
+import com.hcu.hot6.domain.request.PostCreationRequest;
+import com.hcu.hot6.domain.response.PostCreationResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@Rollback
+@Transactional
+public class PostApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @DisplayName("모집글 생성 API 테스트")
+    @Test
+    public void createMentoring() throws Exception{
+
+
+        PostCreationRequest request = new PostCreationRequest();
+        request.setDtype("M");
+        request.setTitle("제목 test");
+        request.setContent("내용 test");
+        request.setContact("연락처 test");
+        request.setPostEnd(LocalDateTime.of(2023, 3, 2, 0, 0, 0));
+        request.setProjectStart(LocalDateTime.of(2023, 3, 10, 0, 0, 0));
+        request.setProjectEnd(LocalDateTime.of(2023, 7, 2, 0, 0, 0));
+        request.setAuthorId(1L);
+        request.setHasPay(true);
+        request.setMaxMentor(0);
+        request.setMaxMentee(4);
+
+        String url = "http://localhost:"+port+"/posts";
+
+        ResponseEntity<PostCreationResponse> response = restTemplate.postForEntity(url, request, PostCreationResponse.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+
+        Assertions.assertThat(response.getBody().getDtype().compareTo("M"));
+        Assertions.assertThat(response.getBody().getTitle().compareTo("제목 test"));
+    }
+
 }
