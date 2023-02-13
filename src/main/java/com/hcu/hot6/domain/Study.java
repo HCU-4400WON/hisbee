14d9acf769bb2a9f95468c619deb826386e61cf0
zionhann <hanzion1108@gmail.com>
2023-02-13 21:08:25 +0900

    :bug: [#37] 중복 메서드 제거

--- src/main/java/com/hcu/hot6/domain/Study.java
+++ src/main/java/com/hcu/hot6/domain/Study.java
@@ -2,10 +2,8 @@ package com.hcu.hot6.domain;
 
 import com.hcu.hot6.domain.request.PostCreationRequest;
 import com.hcu.hot6.domain.request.PostUpdateRequest;
-import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.validation.constraints.Null;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,13 +25,6 @@ public class Study extends Post {
     public Study(PostCreationRequest request, Member author) {
         super(request, author, request.getMaxMember());
         this.maxMember = request.getMaxMember();
-    }
-
-    public void updateStudy(PostUpdateRequest request) {
-        this.maxMember = request.getMaxMember();
-        this.currMember = request.getCurrMember();
-
-        super.updatePost(request, maxMember, currMember);
     }
 
     public void updateStudy(PostUpdateRequest request) {
