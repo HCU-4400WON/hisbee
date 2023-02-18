029dfe60b7ca214ec369cbef07c6e17f6c1f44c1
ohinhyuk <8156217@naver.com>
2023-02-18 16:32:30 +0900

    :art: [#66]optional signup page / design + api connection

--- src/main/frontend/src/Routes/SignUpOptional.tsx
+++ src/main/frontend/src/Routes/SignUpOptional.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 import tw from "tailwind-styled-components";
 
 const Container = tw.div`
@@ -9,7 +12,7 @@ justify-center
 bg-[#bababa]
 `;
 
-const SignUpCard = tw.div`
+const SignUpCard = tw.form`
 w-[800px] 
 bg-[#fff] 
 px-[60px] 
@@ -43,7 +46,6 @@ justify-between
 `;
 const FlexPositionBox = tw(FlexRowBox)`
 justify-evenly
-
 `;
 
 const PositionBox = tw.div`
@@ -52,7 +54,12 @@ const PositionBox = tw.div`
   flex-col
   items-center
   justify-between
-`;
+  border-2
+  border-white
+  opacity-50
+  hover:opacity-100
+  focus:bg-red
+  `;
 
 const PositionGradientBox = tw.span`
 rounded-full 
@@ -60,7 +67,7 @@ w-[80px]
 h-[80px]
 `;
 
-const Info = tw.p`
+const Info = tw.label`
   text-[18px]
   my-[20px]
 `;
@@ -70,6 +77,15 @@ const InfoInput = tw.input`
   h-[35px]
   bg-[#eeeeee]
   rounded-full
+  px-[20px]
+`;
+
+const InfoSelect = tw.select`
+w-[320px]
+h-[35px]
+bg-[#eeeeee]
+rounded-full
+px-[20px]
 `;
 
 const InfoBox = tw.div`
@@ -109,12 +125,14 @@ rounded-full
 `;
 
 function SignUpOptional() {
-  const location = useLocation();
-  console.log(location);
-  const keyword = new URLSearchParams(location.search);
-  console.log(keyword.get("token"));
+  const { register } = useForm();
 
-  localStorage.setItem("key", keyword.get("token") as any);
+  const [positionId, setPositionId] = useState("");
+  const onClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    setPositionId(event.currentTarget.id);
+  };
+
+  console.log(positionId);
 
   return (
     <Container>
@@ -124,26 +142,52 @@ function SignUpOptional() {
         <SubTitle className="mt-[80px]">선택사항</SubTitle>
         <Info>
           * 선택 사항을 기입하시면 인재풀 페이지를 열람 하실 수 있습니다.
-          <button className="float-right w-[100px] border-2 border-red-500 rounded-full text-[red]">
-            나가기
-          </button>
+          <Link to="/">
+            <button className="float-right w-[100px] border-2 border-red-500 rounded-full text-[red]">
+              나가기
+            </button>
+          </Link>
         </Info>
 
         <FlexRequiredBox>
           <div className="flex flex-col justify-evenly h-[400px]">
             <InfoBox>
               <Info>학부</Info>
-              <InfoInput />
+              <InfoSelect {...register("department")}>
+                <option>글로벌리더십학부</option>
+                <option>국제어문학부</option>
+                <option>경영경제학부</option>
+                <option>법학부</option>
+                <option>커뮤니케이션학부</option>
+                <option>공간환경시스템공학부</option>
+                <option>기계제어공학부</option>
+                <option>콘텐츠융합디자인학부</option>
+                <option>생명과학부</option>
+                <option>전산전자공학부</option>
+                <option>상담심리사회복지학부</option>
+                <option>ICT창업학부</option>
+              </InfoSelect>
             </InfoBox>
             <InfoBox>
               <Info>학년</Info>
-              <InfoInput />
+              <InfoSelect {...register("grade")}>
+                <option>1학년</option>
+                <option>2학년</option>
+                <option>3학년</option>
+                <option>4학년</option>
+              </InfoSelect>
             </InfoBox>
 
             <InfoBox>
               <Info>포지션</Info>
               <FlexPositionBox>
-                <PositionBox>
+                <PositionBox
+                  className={`${
+                    positionId === "일반" && "opacity-100 scale-110 z-[100]"
+                  }`}
+                  id="일반"
+                  onClick={onClick}
+                >
                   <PositionGradientBox
                     style={{
                       background:
@@ -152,7 +196,13 @@ function SignUpOptional() {
                   ></PositionGradientBox>
                   <p>일반</p>
                 </PositionBox>
-                <PositionBox>
+                <PositionBox
+                  className={`${
+                    positionId === "기획자" && "opacity-100 scale-110 z-[100]"
+                  }`}
+                  id="기획자"
+                  onClick={onClick}
+                >
                   <PositionGradientBox
                     style={{
                       background:
@@ -161,7 +211,13 @@ function SignUpOptional() {
                   ></PositionGradientBox>
                   <p>기획자</p>
                 </PositionBox>
-                <PositionBox>
+                <PositionBox
+                  className={`${
+                    positionId === "디자이너" && "opacity-100 scale-110 z-[100]"
+                  }`}
+                  id="디자이너"
+                  onClick={onClick}
+                >
                   <PositionGradientBox
                     style={{
                       background:
@@ -170,7 +226,13 @@ function SignUpOptional() {
                   ></PositionGradientBox>
                   <p>디자이너</p>
                 </PositionBox>
-                <PositionBox>
+                <PositionBox
+                  className={`${
+                    positionId === "개발자" && "opacity-100 scale-110 z-[100]"
+                  }`}
+                  id="개발자"
+                  onClick={onClick}
+                >
                   <PositionGradientBox
                     style={{
                       background:
@@ -190,28 +252,31 @@ function SignUpOptional() {
 
         <IntroduceBox>
           <Info className="my-[25px]">자기소개</Info>
-          <IntroduceArea />
+          <IntroduceArea
+            className="p-[20px]"
+            {...register("bio")}
+            placeholder="자유롭게 작성해주세요!"
+          />
         </IntroduceBox>
 
         <FlexRowBox className="justify-between ">
           <InfoBox>
-            <Info className="my-[25px]">동아리 / 학회</Info>
-            <InfoInput />
+            <Info className="my-[25px]">동아리 / 학회 1</Info>
+            <InfoInput {...register("club1")} />
           </InfoBox>
-          {/* <InfoBox>
-            <Info className="my-[25px]">이메일 주소</Info>
-            <InfoInput />
-          </InfoBox> */}
+          <InfoBox>
+            <Info className="my-[25px]">동아리 / 학회 2</Info>
+            <InfoInput {...register("club2")} />
+          </InfoBox>
         </FlexRowBox>
-
         <FlexRowBox className="justify-between">
           <InfoBox>
             <Info className="my-[25px]">연락수단</Info>
-            <InfoInput />
+            <InfoInput placeholder="ex) email or phone" />
           </InfoBox>
           <InfoBox>
             <Info className="my-[25px]">외부링크</Info>
-            <InfoInput />
+            <InfoInput placeholder="ex) github or Linked-In" />
           </InfoBox>
         </FlexRowBox>
 
