d285fe16d53c370a5c90b6577a3b548a286e7a90
ohinhyuk <8156217@naver.com>
2023-02-20 01:35:09 +0900

    :art: [#70] 모집글 , 메인 페이지 디자인 수정
    모집글 , 메인 페이지 디자인 + 프로필 페이지 모집글 디자인 수정

--- src/main/frontend/src/Routes/Post.tsx
+++ src/main/frontend/src/Routes/Post.tsx
@@ -60,18 +60,18 @@ const SortSelect = tw.select`
 `;
 
 const PostGrid = tw.div`
-grid 
- grid-cols-1
- sm:grid-cols-2
- xl:grid-cols-4
-gap-10
-
+grid
+grid-cols-1
+sm:grid-cols-2
+xl:grid-cols-4
+pb-[100px]
 `;
 
 const PostItem = tw.div`
 relative
+justify-self-center
 h-[210px] 
-w-[340px]
+min-w-[330px]
 rounded-md
 overflow-hidden
 `;
@@ -92,6 +92,74 @@ items-center
 bg-gray-500
 p-[15px]
 `;
+
+const PostCategorySpan = tw.span`
+text-[#185ee4] 
+bg-[#fff] 
+h-[25px] 
+border 
+w-[80px] 
+text-[15px] 
+font-bold 
+rounded-full 
+flex 
+items-center 
+justify-center
+`;
+
+const PostCategoryLabel = tw.label`
+`;
+
+const HeartIcon = tw.i`
+`;
+
+const PostMainPart = tw.div`
+bg-[#e9e9eb] 
+w-full 
+h-full 
+px-[25px] 
+py-[15px]
+`;
+
+const PostTitle = tw.p`
+text-lg 
+font-semibold
+`;
+const PostDate = tw.div`
+flex text-[12px] 
+font-semibold 
+items-center
+`;
+
+const PostDatePlan = tw.p``;
+const PostDateStart = tw.p``;
+
+const PostPerson = tw.div`
+absolute 
+left-[25px] 
+bottom-[15px] 
+flex 
+items-center 
+gap-2
+`;
+
+const PostPersonTotal = tw.p`
+text-[#185ee4] 
+font-bold 
+text-[14px]
+`;
+
+const PostPersonPosition = tw.span`
+border-gray-400 
+border 
+rounded-full 
+px-[10px] 
+text-[11px] 
+text-gray-500 
+font-medium
+`;
+
+const Container = tw.div``;
 
 function Post() {
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
@@ -159,8 +227,10 @@ function Post() {
     프로젝트: ["있음", "없음"],
   };
 
+  const windowPx = window.innerWidth;
+  console.log(windowPx, "px");
   return (
-    <>
+    <Container>
       {/* <img
         className="h-60 
 mt-5 
@@ -241,7 +311,7 @@ bg-gray-200"
         </SortSelect>
       </SortBox>
 
-      <PostGrid className="">
+      <PostGrid>
         {posts.map((post, index) => (
           <PostItem
             key={index}
@@ -256,16 +326,17 @@ bg-gray-200"
                   : "bg-[#bdc9f2]"
               }`}
             >
-              <span className="text-[#185ee4] bg-[#fff] h-[25px] border w-[80px] text-[15px] font-bold rounded-full flex items-center justify-center">
-                <label>
+              <PostCategorySpan>
+                <PostCategoryLabel>
                   {post.dtype === "P"
                     ? "프로젝트"
                     : post.dtype === "S"
                     ? "스터디"
                     : "멘토링"}
-                </label>
-              </span>
-              <svg
+                </PostCategoryLabel>
+              </PostCategorySpan>
+              <HeartIcon className="fa-regular fa-heart"></HeartIcon>
+              {/* <svg
                 width="15px"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
@@ -274,129 +345,126 @@ bg-gray-200"
                   stroke="1"
                   d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z"
                 />
-              </svg>
+              </svg> */}
               {/* <p className="mx-5 my-1 text-sm font-bold">개발자</p>
        <p className="text-sm text-blue-500">{post.total}명 모집</p> */}
             </PostContentFirstRow>
 
-            <div
-              id="restPart"
-              className="bg-[#e9e9eb] w-full h-full px-[25px] py-[15px]"
-            >
+            <PostMainPart>
               {/* secondRow */}
-              <p className="text-lg font-semibold">
+              <PostTitle className="text-lg font-semibold">
                 {post.title.length > 20
                   ? post.title.slice(0, 20) + "..."
                   : post.title}
-              </p>
+              </PostTitle>
 
               {/* ThirdRow */}
-              <div className="flex text-[12px] font-semibold items-center">
+              <PostDate>
                 {(post.projectEnd.getTime() - post.projectStart.getTime()) /
                   (1000 * 24 * 60 * 60) >=
                 365 ? (
-                  <p>
+                  <PostDatePlan>
                     {Math.floor(
                       (post.projectEnd.getTime() -
                         post.projectStart.getTime()) /
                         (1000 * 24 * 60 * 60 * 365)
                     )}
                     {""}년 플랜
-                  </p>
+                  </PostDatePlan>
                 ) : (post.projectEnd.getTime() - post.projectStart.getTime()) /
                     (1000 * 24 * 60 * 60) >=
                   30 ? (
-                  <p>
+                  <PostDatePlan>
                     {Math.floor(
                       (post.projectEnd.getTime() -
                         post.projectStart.getTime()) /
                         (1000 * 24 * 60 * 60 * 30)
                     )}
                     {""}달 플랜
-                  </p>
+                  </PostDatePlan>
                 ) : (post.projectEnd.getTime() - post.projectStart.getTime()) /
                     (1000 * 24 * 60 * 60) >=
                   7 ? (
-                  <p>
+                  <PostDatePlan>
                     {Math.floor(
                       (post.projectEnd.getTime() -
                         post.projectStart.getTime()) /
                         (1000 * 24 * 60 * 60 * 7)
                     )}
                     {""}주 플랜
-                  </p>
+                  </PostDatePlan>
                 ) : (
-                  <p>
+                  <PostDatePlan>
                     {Math.floor(
                       (post.projectEnd.getTime() -
                         post.projectStart.getTime()) /
                         (1000 * 24 * 60 * 60)
                     )}
                     {""}일 플랜
-                  </p>
+                  </PostDatePlan>
                 )}
                 <p className="mx-[7px] pb-0.5">|</p>
-                <p>
+                <PostDateStart>
                   {" "}
                   {post.projectStart.getMonth()}월 {post.projectStart.getDate()}
                   일 시작
-                </p>
-              </div>
+                </PostDateStart>
+              </PostDate>
 
               {/* lastRow */}
-              <div className="absolute left-[25px] bottom-[15px] flex items-center gap-2">
-                <p className=" text-[#185ee4] font-bold text-[14px]">
+              <PostPerson>
+                <PostPersonTotal>
                   {post.dtype === "P"
                     ? post.maxDesigner + post.maxDeveloper + post.maxPlanner
                     : post.dtype === "S"
                     ? post.maxMember
                     : post.maxMentee + post.maxMentor}
                   명 모집
-                </p>
+                </PostPersonTotal>
 
                 {post.dtype === "P" ? (
                   <>
                     {post.maxDeveloper !== 0 && (
-                      <span className="border-gray-400 border rounded-full px-[10px] text-[11px] text-gray-500 font-medium">
+                      <PostPersonPosition>
                         개발자 {post.maxDeveloper}명
-                      </span>
+                      </PostPersonPosition>
                     )}
                     {post.maxPlanner !== 0 && (
-                      <span className="border-gray-400 border rounded-full px-[10px] text-[11px] text-gray-500 font-medium">
+                      <PostPersonPosition>
                         기획자 {post.maxPlanner}명
-                      </span>
+                      </PostPersonPosition>
                     )}
 
                     {post.maxDesigner !== 0 && (
-                      <span className="border-gray-400 border rounded-full px-[10px] text-[11px] text-gray-500 font-medium">
+                      <PostPersonPosition>
                         디자이너 {post.maxDesigner}명
-                      </span>
+                      </PostPersonPosition>
                     )}
                   </>
                 ) : post.dtype === "S" ? (
-                  <span className="border-gray-400 border rounded-full px-[10px] text-[11px] text-gray-500 font-medium">
+                  <PostPersonPosition>
                     스터디원 {post.maxMember}명
-                  </span>
+                  </PostPersonPosition>
                 ) : (
                   <>
                     {post.maxMentor !== 0 && (
-                      <span className="border-gray-400 border rounded-full px-[10px] text-[11px] text-gray-500 font-medium">
+                      <PostPersonPosition>
                         멘토 {post.maxMentor}명
-                      </span>
+                      </PostPersonPosition>
                     )}
                     {post.maxMentee !== 0 && (
-                      <span className="border-gray-400 border rounded-full px-[10px] text-[11px] text-gray-500 font-medium">
+                      <PostPersonPosition>
                         멘티 {post.maxMentee}명
-                      </span>
+                      </PostPersonPosition>
                     )}
                   </>
                 )}
-              </div>
-            </div>
+              </PostPerson>
+            </PostMainPart>
           </PostItem>
         ))}
       </PostGrid>
-    </>
+    </Container>
   );
 }
 
