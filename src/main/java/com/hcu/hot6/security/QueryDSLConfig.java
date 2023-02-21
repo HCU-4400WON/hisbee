4779ef2967f4d3dc2589af9e1b336cd0351f3ddd
zionhann <hanzion1108@gmail.com>
2023-02-21 15:33:55 +0900

    :wrench: [#75] 동적쿼리 생성을 위한 QueryDSL 의존성 추가

---
+++ src/main/java/com/hcu/hot6/security/QueryDSLConfig.java
@@ -0,0 +1,19 @@
+package com.hcu.hot6.security;
+
+import com.querydsl.jpa.impl.JPAQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDSLConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQuery<?> jpaQuery() {
+        return new JPAQuery<Void>(entityManager);
+    }
+}
