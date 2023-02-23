515111068947b07a8e75c88792373274ca0fc0c1
zionhann <hanzion1108@gmail.com>
2023-02-23 15:18:48 +0900

    :sparkles: [#85] 모집글 찜 추가/삭제

--- src/main/java/com/hcu/hot6/PostApiController.java
+++ src/main/java/com/hcu/hot6/PostApiController.java
@@ -1,9 +1,11 @@
 package com.hcu.hot6;
 
+import com.hcu.hot6.domain.Member;
 import com.hcu.hot6.domain.Post;
 import com.hcu.hot6.domain.SearchInfo;
 import com.hcu.hot6.domain.request.PostCreationRequest;
 import com.hcu.hot6.domain.request.PostUpdateRequest;
+import com.hcu.hot6.domain.response.MemberResponse;
 import com.hcu.hot6.domain.response.PostCreationResponse;
 import com.hcu.hot6.domain.response.PostReadOneResponse;
 import com.hcu.hot6.service.PostService;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -67,9 +70,9 @@ public class PostApiController {
 
     /**
      * 모집글 필터링 전체 조회
-     * */
+     */
     @GetMapping("/posts")
-    public ResponseEntity<List<Post>> readFilteredPost(@RequestParam int page, @RequestParam(required = false) String search, @RequestParam String order, @RequestParam(required = false) String type, @RequestParam(required = false) String position, @RequestParam(required = false) boolean pay, @RequestParam int limit){
+    public ResponseEntity<List<Post>> readFilteredPost(@RequestParam int page, @RequestParam(required = false) String search, @RequestParam String order, @RequestParam(required = false) String type, @RequestParam(required = false) String position, @RequestParam(required = false) boolean pay, @RequestParam int limit) {
         SearchInfo searchInfo = SearchInfo.builder()
                 .page(page)
                 .search(search)
@@ -80,5 +83,23 @@ public class PostApiController {
                 .limit(limit)
                 .build();
         return ResponseEntity.ok(postService.readFilteredPost(searchInfo));
+    }
+
+    @PostMapping("/posts/{postId}/likes")
+    public ResponseEntity<List<MemberResponse>> doBookmark(@PathVariable Long postId,
+                                                           @AuthenticationPrincipal OAuth2User user) {
+        List<Member> members = postService.addBookmark(postId, user.getName());
+        return ResponseEntity.ok(members.stream()
+                .map(MemberResponse::new)
+                .collect(Collectors.toList()));
+    }
+
+    @DeleteMapping("/posts/{postId}/likes")
+    public ResponseEntity<List<MemberResponse>> undoBookmark(@PathVariable Long postId,
+                                                             @AuthenticationPrincipal OAuth2User user) {
+        List<Member> members = postService.delBookmark(postId, user.getName());
+        return ResponseEntity.ok(members.stream()
+                .map(MemberResponse::new)
+                .collect(Collectors.toList()));
     }
 }
