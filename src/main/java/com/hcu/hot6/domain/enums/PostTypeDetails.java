4241fc26c88b0ec31cfa180679bf896f53a3e799
zionhann <hanzion1108@gmail.com>
2023-02-27 21:38:21 +0900

    :sparkles: [#63] 필터링을 위한 enum 생성

---
+++ src/main/java/com/hcu/hot6/domain/enums/PostTypeDetails.java
@@ -0,0 +1,14 @@
+package com.hcu.hot6.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum PostTypeDetails {
+    PLANNER(PostType.PROJECT), DESIGNER(PostType.PROJECT), DEVELOPER(PostType.PROJECT),
+    MENTOR(PostType.MENTORING), MENTEE(PostType.MENTORING),
+    MEMBER(PostType.STUDY);
+
+    private final PostType type;
+}
