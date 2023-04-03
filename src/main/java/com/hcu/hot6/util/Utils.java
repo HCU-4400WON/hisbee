d3494e0853cb66dfd06b3642a500952a17788545
zionhann <hanzion1108@gmail.com>
2023-04-03 22:34:37 +0900

    :sparkles: [#137] 문자열<->리스트 간 유틸 메서드 추가

--- src/main/java/com/hcu/hot6/util/Utils.java
+++ src/main/java/com/hcu/hot6/util/Utils.java
@@ -3,6 +3,8 @@ package com.hcu.hot6.util;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 public class Utils {
     public static LocalDateTime toLocalDateTime(Date e) {
@@ -11,5 +13,17 @@ public class Utils {
 
     public static Date toDate(LocalDateTime e) {
         return java.sql.Timestamp.valueOf(e);
+    }
+
+    public static List<String> toArray(String s) {
+        return List.of(s.split(","));
+    }
+
+    public static String toString(List<String> list) {
+        return String.join(
+                ",",
+                Optional.ofNullable(list)
+                        .orElse(List.of())
+        );
     }
 }
