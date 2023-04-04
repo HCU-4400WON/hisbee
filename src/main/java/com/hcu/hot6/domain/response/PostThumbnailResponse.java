0bbb4f35fc70666c8c7b566db9ba9314abe34da8
zionhann <hanzion1108@gmail.com>
2023-04-04 13:53:02 +0900

    :art: 응답폼 필드명/순서 변경

--- src/main/java/com/hcu/hot6/domain/response/PostThumbnailResponse.java
+++ src/main/java/com/hcu/hot6/domain/response/PostThumbnailResponse.java
@@ -1,37 +1,38 @@
 package com.hcu.hot6.domain.response;
 
 import com.hcu.hot6.domain.Thumbnail;
-import com.hcu.hot6.util.Utils;
 import lombok.Getter;
 
 import java.util.Date;
 import java.util.List;
 
+import static com.hcu.hot6.util.Utils.toArray;
+import static com.hcu.hot6.util.Utils.toDate;
+
 @Getter
 public class PostThumbnailResponse {
-
     private Long id;
-    private List<String> postTypes;
     private String title;
     private String summary;
-    private String tags;
-    private Long views;
-    private Date createdDate;
-    private boolean hasLiked;
     private Date recruitStart;
     private Date recruitEnd;
+    private List<String> postTypes;
+    private List<String> tags;
+    private Long views;
+    private boolean hasLiked;
+    private Date createdDate;
 
     public PostThumbnailResponse(final Thumbnail thumbnail, String email) {
         this.id = thumbnail.getPost().getId();
-        this.postTypes = Utils.toArray(thumbnail.getPost().getPostTypes(), ",");
         this.title = thumbnail.getTitle();
         this.summary = thumbnail.getSummary();
-        this.tags = thumbnail.getTags();
+        this.postTypes = toArray(thumbnail.getPost().getPostTypes(), ",");
+        this.tags = toArray(thumbnail.getTags(), ";");
         this.views = thumbnail.getPost().getViews();
-        this.createdDate = Utils.toDate(thumbnail.getPost().getCreatedDate());
+        this.createdDate = toDate(thumbnail.getPost().getCreatedDate());
+        this.recruitStart = toDate(thumbnail.getRecruitStart());
+        this.recruitEnd = toDate(thumbnail.getRecruitEnd());
         this.hasLiked = thumbnail.getPost().getBookmarks().stream()
                 .anyMatch(bookmark -> email.equals(bookmark.getMember().getEmail()));
-        this.recruitStart = Utils.toDate(thumbnail.getRecruitStart());
-        this.recruitEnd = Utils.toDate(thumbnail.getRecruitEnd());
     }
 }
