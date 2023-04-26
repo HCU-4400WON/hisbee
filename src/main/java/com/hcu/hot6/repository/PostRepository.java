2cba8bf43521ac891372754118378bdfae8581ad
jang-yujin <jyj9747@naver.com>
2023-04-27 00:17:02 +0900

    :sparkles: [#189] 전공 필터링 구현
    학부가 아닌 전공만 등록한 모집글도 필터링 됨.

--- src/main/java/com/hcu/hot6/repository/PostRepository.java
+++ src/main/java/com/hcu/hot6/repository/PostRepository.java
@@ -13,10 +13,10 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -106,7 +106,78 @@ public class PostRepository {
     private BooleanBuilder eqDepartment(String department) {
         var builder = new BooleanBuilder();
 
-        return builder.or(post.targetDepartment.contains(department));
+        List<String> majors = new ArrayList<>();
+        if(department.compareTo("ICT창업학부") == 0){
+            majors.add("GE");
+            majors.add("ICT융합");
+            majors.add("ACE");
+        }
+        else if(department.compareTo("국제어문학부") == 0){
+            majors.add("국제지역학");
+            majors.add("영어");
+        }
+        else if(department.compareTo("생명공학부") == 0){
+            majors.add("생명공학부");
+        }
+        else if(department.compareTo("경영경제학부") == 0){
+            majors.add("경영학");
+            majors.add("경제학");
+            majors.add("GM");
+        }
+        else if(department.compareTo("법학부") == 0){
+            majors.add("한국법");
+            majors.add("UIL");
+        }
+        else if(department.compareTo("상담심리사회복지학부") == 0){
+            majors.add("상담심리학");
+            majors.add("사회복지학");
+        }
+        else if(department.compareTo("커뮤니케이션학부") == 0){
+            majors.add("언론정보학");
+            majors.add("공연영상학");
+        }
+        else if(department.compareTo("공간환경시스템공학부") == 0){
+            majors.add("건설공학");
+            majors.add("도시환경공학");
+        }
+        else if(department.compareTo("기계제어공학부") == 0){
+            majors.add("기계공학");
+            majors.add("전자제어공학");
+        }
+        else if(department.compareTo("전산전자공학부") == 0){
+            majors.add("AI컴퓨터공학심화");
+            majors.add("컴퓨터공학");
+            majors.add("전자공학심화");
+            majors.add("전자공학");
+            majors.add("IT");
+
+        }
+        else if(department.compareTo("콘텐츠융합디자인학부") == 0){
+            majors.add("시각디자인");
+            majors.add("제품디자인");
+        }
+
+        if(department.compareTo("ICT창업학부") == 0 || department.compareTo("경영경제학부") == 0){
+            return builder
+                    .or(post.targetDepartment.contains(department))
+                    .or(post.targetDepartment.contains(majors.get(0)))
+                    .or(post.targetDepartment.contains(majors.get(1)))
+                    .or(post.targetDepartment.contains(majors.get(2)));
+        }
+        else if(department.compareTo("전산전자공학부") == 0){
+            return builder
+                    .or(post.targetDepartment.contains(department))
+                    .or(post.targetDepartment.contains(majors.get(0)))
+                    .or(post.targetDepartment.contains(majors.get(1)))
+                    .or(post.targetDepartment.contains(majors.get(2)))
+                    .or(post.targetDepartment.contains(majors.get(3)))
+                    .or(post.targetDepartment.contains(majors.get(4)));
+        }
+
+        return builder
+                .or(post.targetDepartment.contains(department))
+                .or(post.targetDepartment.contains(majors.get(0)))
+                .or(post.targetDepartment.contains(majors.get(1)));
     }
 
     private OrderSpecifier<?> orderCond(OrderBy orderBy) {
