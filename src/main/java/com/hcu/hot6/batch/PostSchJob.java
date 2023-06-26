4ddb5652a5bd25253c0446af726aebfe321c50d1
jang-yujin <jyj9747@naver.com>
2023-06-26 22:55:23 +0900

    :poop: Spring batch job, quartz 스케쥴러 사용 시도
    기간 지남에 따라 자동 모집 완료 구현하기 위함

---
+++ src/main/java/com/hcu/hot6/batch/PostSchJob.java
@@ -0,0 +1,32 @@
+package com.hcu.hot6.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostSchJob extends QuartzJobBean {
+
+    private final Job postJob;
+    private final JobLauncher jobLauncher;
+    private final JobExplorer jobExplorer;
+
+    /**
+     * 배치를 실행시키는 구문 : 스케줄링된 이벤트가 발생할때마다 한번씩 호출된다.
+     * @param context
+     * @throws JobExecutionException
+     */
+
+    @SneakyThrows
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+
+    }
+}
