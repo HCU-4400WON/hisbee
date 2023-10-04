4e5d120cb85d34957cda4a257706118991588740
ohinhyuk <8156217@naver.com>
2023-10-04 20:31:20 +0900

    HotFix : 회원 가입 시 생성자 버그 및 배치 설정이 올바르지 않은 버그 수정

--- src/main/java/com/hcu/hot6/domain/request/MemberRequest.java
+++ src/main/java/com/hcu/hot6/domain/request/MemberRequest.java
@@ -2,11 +2,15 @@ package com.hcu.hot6.domain.request;
 
 import com.hcu.hot6.domain.enums.Major;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberRequest {
 
     @NotNull private String nickname;
